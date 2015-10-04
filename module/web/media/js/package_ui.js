0210e2d3d6ccba7a9ddef2de39dfd748c3ae56fc
dischinator <dischinator@users.noreply.github.com>
2015-10-04 16:13:08 +0200

    Add mootools code to toggle bootstrap navigation menu.

--- module/web/media/js/package_ui.js
+++ module/web/media/js/package_ui.js
@@ -12,6 +12,18 @@ document.addEvent("domready", function() {
         $('pack_form').reset();
         root.packageBox.close();
     });
+
+    $$('.navbar-toggle').addEvent('click', function(e) {
+        var button;
+        if(e.target.hasClass('navbar-toggle')) {
+            button = e.target;
+        } else {
+            button = e.target.getParent('.navbar-toggle');
+        }
+        var toggle = button.getProperty('data-toggle');
+        var target = button.getProperty('data-target');
+        $$(target).toggleClass(toggle);
+    });
 });
 
 function indicateLoad() {
